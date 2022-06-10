7fbbf63f4c9c55f2a6e729ae91c45e338c1e02aa
Samir Trentini Zedan <samir.tzedan@gmail.com>
2022-06-10 08:37:04 -0300

    Melhorias Card

--- resources/views/components/status-pokemon.blade.php
+++ resources/views/components/status-pokemon.blade.php
@@ -3,8 +3,8 @@
         {{ $nome }}
     </div>
     <div class="col-9">
-        <div class="progress">
-            <div class="progress-bar" role="progressbar" style="width: {{ ($porcentagem*100)/150 }}%" aria-valuenow="{{ $porcentagem }}" aria-valuemin="0" aria-valuemax="100"></div>
+        <div class="progress mt-1">
+            <div class="progress-bar" role="progressbar" style="width: {{ ($valor*100)/150 }}%" aria-valuenow="{{ $valor }}" aria-valuemin="0" aria-valuemax="100">{{ $valor }}</div>
         </div>
     </div>
 </div>
--- resources/views/pokemon/card.blade.php
+++ resources/views/pokemon/card.blade.php
@@ -1,7 +1,12 @@
 <x-layout title="{{ $pokemon->name }}">
-    <a href="{{ URL::previous() }}" class="btn btn-primary my-2"><i class="fa-solid fa-arrow-left"></i></a>
+    <div class="row">
+        <div class="col">
+            <a href="{{ route('pokemon.index') }}" class="btn btn-primary my-2"><i class="fa-solid fa-arrow-left"></i></a>
+            <a href="{{ route('pokemon.edit', $pokemon) }}" class="btn btn-primary my-2 ms-2">Editar</a>
+        </div>
+    </div>
     <div class="container row my-3 py-4 px-4 rounded text-light bg-transparent-10">
-        <div class="col-2">
+        <div class="col-2 ps-0 pe-3">
             <img
                 src="{{ $pokemon->getImage(1, $pokemon->numero) }}"
                 alt="{{ $pokemon->name }}"
@@ -9,12 +14,12 @@
             >
         </div>
         <div class="col-6 bg-light text-dark rounded p-2">
-            <x-status-pokemon nome="HP" porcentagem="{{ $pokemon->hp }}"/>
-            <x-status-pokemon nome="Attack" porcentagem="{{ $pokemon->attack }}"/>
-            <x-status-pokemon nome="Defense" porcentagem="{{ $pokemon->defense }}"/>
-            <x-status-pokemon nome="Sp. Attack" porcentagem="{{ $pokemon->special_attack }}"/>
-            <x-status-pokemon nome="Sp. Defense" porcentagem="{{ $pokemon->special_defense }}"/>
-            <x-status-pokemon nome="Speed" porcentagem="{{ $pokemon->speed }}"/>
+            <x-status-pokemon nome="HP" valor="{{ $pokemon->hp }}"/>
+            <x-status-pokemon nome="Attack" valor="{{ $pokemon->attack }}"/>
+            <x-status-pokemon nome="Defense" valor="{{ $pokemon->defense }}"/>
+            <x-status-pokemon nome="Sp. Attack" valor="{{ $pokemon->special_attack }}"/>
+            <x-status-pokemon nome="Sp. Defense" valor="{{ $pokemon->special_defense }}"/>
+            <x-status-pokemon nome="Speed" valor="{{ $pokemon->speed }}"/>
         </div>
     </div>
 </x-layout>
